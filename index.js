f845ff18fa8a3118f4a2580af69262d73a69b9ab
Preetinder Singh <preetindersingh13per@gmail.com>
2024-11-04 00:53:52 +0530

    Jwt Auth Added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const authService = require('./services/auth');
 require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(MONGO_URL);
+const database = mongoose.connection;
 
 app.use(cors({
     origin: ['http://localhost:3000']
@@ -22,6 +31,16 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     checkApikey(req, res, next);
 });
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+app.use('/auth', authService);
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT} ....`)
---
+++ services/auth.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, email, password: hashedPassword });
+        await user.save();
+        res.status(200).json({
+            message: 'User Successfully Added'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Registration Failed'
+        })
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ error: "Authentication failed" });
+        }
+        const passwordMatched = await bcrypt.compare(password, user.password);
+        if (!passwordMatched) {
+            return res.status(401).json({ error: "Authentication failed" });
+        }
+        const token = jwt.sign(
+            { userId: user._id },
+            JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        res.status(200).json({token: token});
+    } catch (error) {
+        res.status(500).json({error: "Login failed"});
+    }
+});
+
+module.exports = router;
