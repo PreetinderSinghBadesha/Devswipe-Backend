9df6e05cf6d6466eff1ca68750b72462a89cbfd8
Lakshay Singla <lsingla1_be23@thapar.edu>
2024-11-13 17:23:01 +0530

    Members add remove api added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,19 +4,38 @@ const User = require('../models/User');
 const Project = require('../models/Project');
 const verifyToken = require('../middlewares/jwtMiddleware');
 
-router.post('/add-members/:projectId',verifyToken, async (req,res) =>{
+router.post('/add-members/:projectId',verifyToken, async (req,res,next) =>{
     try{
-        const user = req.body;
+        const {members} = req.body;
+
         await Project.findByIdAndUpdate(
             req.params.projectId,
-            { $addToSet: { : projectId } },
+            { $addToSet: { members: { $each: members } } },
             { new: true }
         );
-
+        res.status(200).json({ message: 'member added'});
 
     } catch (error) {
         res.status(500).json({ error: "Failed to add member" });
     }
 });
 
+router.post('/remove-members/:projectId',verifyToken, async (req,res,next) =>{
+    try{
+        const {members} = req.body;
+
+        await Project.findByIdAndUpdate(
+            req.params.projectId,
+            { $pull: { members: { _id: { $in: members } } } },
+            { new: true }
+        );
+        res.status(200).json({ message: 'member removed'});
+
+    } catch (error) {
+        res.status(500).json({ 
+            error: "Failed to remove member",
+            details: error.message
+        });
+    }
+});
 module.exports = router;
